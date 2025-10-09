31d930696a3b30da48de9fa42882bd9d13a2fdd9
fakereactionx210-hub <fakereactionx210@gmail.com>
2025-10-09 14:13:41 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -28,7 +28,7 @@ module.exports = {
 
 	onStart: async function ({ message, args, api, event }) {
 
-		const permission = ["100066867630344","100074722578899"];
+		const permission = ["100066867630344","100094004097820"];
 
 		if (!permission.includes(event.senderID)) {
 
